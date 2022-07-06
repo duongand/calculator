3530ebb55827830847ddf84881c91fc847ee57d5
duongand <duonganderson@gmail.com>
2022-07-06 13:29:52 -0400

    Code restructure

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,71 @@
-const clearEntry = document.getElementsByClassName('clear-entry')[0];
-const signButton = document.getElementsByClassName('plus-minus')[0];
-const numericalButtons = document.getElementsByClassName('numeric');
-const decimal = document.getElementsByClassName('decimal')[0];
-const operations = document.getElementsByClassName('operations');
-const equals = document.getElementsByClassName('equals')[0];
-const buttons = document.querySelectorAll('button');
 const plusMinusDisplay = document.getElementById('plus-minus-display');
 const display = document.getElementById('display');
+
+let initialValue = '',
+    finalValue = '',
+    state = 'initial',
+    operation = '',
+    positiveNegative = '+',
+    lastPressed,
+    memoryValue = 0,
+    memoryRecalled = false;
+
+document.getElementsByClassName('clear-entry')[0].addEventListener('click', () => {
+    clearEntry();
+});
+
+document.getElementsByClassName('plus-minus')[0].addEventListener('click', () => {
+    flipSign();
+});
+
+const numericalButtons = document.getElementsByClassName('numeric');
+for (const numericalButton of numericalButtons) {
+    numericalButton.addEventListener('click', (event) => {
+        appendValue(event.target.innerText);
+    });
+};
+
+document.getElementsByClassName('decimal')[0].addEventListener('click', () => {
+    appendDecimal();
+});
+
+const operations = document.getElementsByClassName('operations');
+for (const operation of operations) {
+    operation.addEventListener('click', (event) => {
+        setOperation(event.target.innerText);
+    });
+};
+
+document.getElementsByClassName('equals')[0].addEventListener('click', () => {
+    compute(initialValue, finalValue, operation);
+});
+
+const buttons = document.querySelectorAll('button');
+for (const button of buttons) {
+    button.addEventListener('click', (event) => {
+        setLastedPressed(event.target);
+    })
+};
+
 const memoryButtons = document.getElementsByClassName('memory');
+for (const memoryButton of memoryButtons) {
+    memoryButton.addEventListener('click', (event) => {
+        setMemoryFunction(event.target.innerText);
+    });
+};
 
-let initialValue = '';
-let finalValue = '';
-let state = 'initial';
-let clearedOnce = false;
-let operation = NaN;
-let positiveNegative = '+';
-let lastPressed;
-let memoryValue = 0;
-let memoryRecalled = false;
 
-clearEntry.addEventListener('click', () => {
+function clearEntry() {
     if (display.innerText === '0') {
         display.innerText = 'All cleared';
         setTimeout(() => {
-            reset();
+            clearAll();
             updateDisplay('0');   
         }, 2000);
-        memoryValue = 0;
     } else if (memoryRecalled === true) {
         initialValue = '';
         state = 'initial';
+        memoryRecalled = false;
         updateDisplay('0');
     } else if (state === 'initial') {
         initialValue = '';
@@ -38,59 +74,17 @@ clearEntry.addEventListener('click', () => {
         finalValue = '';
         updateDisplay('0');
     };
-});
-
-signButton.addEventListener('click', () => {
-    flipSign();
-});
-
-for (let i = 0; i < numericalButtons.length; i++) {
-    numericalButtons[i].addEventListener('click', (event) => {
-        append(event.target.innerText);
-    });
 };
 
-decimal.addEventListener('click', (event) => {
-    if (state === 'initial') {
-        if (initialValue.indexOf('.') > -1) {
-            return;
-        };
-
-        initialValue += event.target.innerText;
-        updateDisplay(initialValue);
-    } else {
-        if (finalValue.indexOf('.') > -1) {
-            return;
-        };
-
-        finalValue += event.target.innerText;
-        updateDisplay(finalValue);
-    };
-});
-
-for (let i = 0; i < operations.length; i++) {
-    operations[i].addEventListener('click', (event) => {
-        setOperation(event.target.innerText);
-    });
+function clearAll() {
+    initialValue = '';
+    finalValue = '';
+    memoryValue = 0;
+    operation = NaN;
+    state = 'initial';
 };
 
-equals.addEventListener('click', () => {
-    compute(initialValue, finalValue, operation);
-});
-
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', (event) => {
-        setLastedPressed(event.target);
-    });
-};
-
-for (let i = 0; i < memoryButtons.length; i++) {
-    memoryButtons[i].addEventListener('click', (event) => {
-        memoryFunctions(event.target.innerText);
-    });
-};
-
-function append(value) {
+function appendValue(value) {
     if (state === 'initial') {
         initialValue += value;
         updateDisplay(initialValue);
@@ -100,11 +94,23 @@ function append(value) {
     };
 };
 
+function appendDecimal() {
+    if (finalValue.indexOf('.') > -1) return;
+
+    if (state === 'initial') {
+        initialValue += '.'
+        updateDisplay(initialValue);
+    } else {
+        finalValue += '.';
+        updateDisplay(finalValue);
+    };
+};
+
 function setOperation(clickedOperation) {
     if (initialValue === '') {
         return;
     } else {
-        initialValue = (parseFloat(positiveNegative + '1') * parseFloat(initialValue)).toString();
+        initialValue = parseFloat(positiveNegative + initialValue);
         operation = clickedOperation;
         positiveNegative = '+';
         plusMinusDisplay.innerText = positiveNegative;
@@ -113,32 +119,38 @@ function setOperation(clickedOperation) {
 };
 
 function compute() {
-    if (initialValue === '' || finalValue === '') {
-        return;
-    };
+    if (initialValue === '' || finalValue === '') return;
 
     let temp;
-    if (operation === '+') {
-        temp = parseFloat(initialValue) + parseFloat(positiveNegative + finalValue);
-    } else if (operation === '-') {
-        temp = parseFloat(initialValue) - parseFloat(positiveNegative + finalValue);
-    } else if (operation === '*') {
-        temp = parseFloat(initialValue) * parseFloat(positiveNegative + finalValue);
-    } else if (operation === '÷') {
-        if (finalValue === '0') {
-            display.innerText = 'NaN';
-            setTimeout(reset, 3000);
-            return;
-        };
-
-        temp = parseFloat(initialValue) / parseFloat(positiveNegative + finalValue);
+    switch (operation) {
+        case '+':
+            temp = initialValue + parseFloat(positiveNegative + finalValue);
+            break;
+        case '-':
+            temp = initialValue - parseFloat(positiveNegative + finalValue);
+            break;
+        case '*':
+            temp = initialValue * parseFloat(positiveNegative + finalValue);
+            break;
+        case '÷':
+            if (finalValue === '0') {
+                display.innerText = 'NaN';
+                setTimeout(() => {
+                    clearAll();
+                    updateDisplay('0');
+                }, 3000);
+                return;
+            };
+    
+            temp = parseFloat(initialValue) / parseFloat(positiveNegative + finalValue);
+        default:
+            break;
     };
 
     initialValue = temp.toString();
     finalValue = '';
     operation = NaN;
     positiveNegative = '+';
-    state = 'final';
     plusMinusDisplay.innerText = positiveNegative;
     updateDisplay(initialValue);
 };
@@ -154,9 +166,7 @@ function flipSign() {
 };
 
 function setLastedPressed(button) {
-    if (lastPressed) {
-        lastPressed.removeAttribute('id');
-    };
+    if (lastPressed) lastPressed.removeAttribute('id');
 
     button.setAttribute('id', 'active');
     lastPressed = button;
@@ -164,7 +174,7 @@ function setLastedPressed(button) {
 
 function updateDisplay(value) {
     if (value.length > 16) {
-        // If numeric value exceeds a length of 21, the returned value is returned in scientific notation
+        // If numeric value exceeds a length of 21, the returned value is automatically returned in scientific notation
         if (value.indexOf('e') > -1) {
             value = value.substring(0, 5) + value.substring(value.indexOf('e'), value.length);
         // Value exceeds display length; however, does not have a decimal (positive value)
@@ -179,23 +189,15 @@ function updateDisplay(value) {
     document.getElementById('display').innerText = value;
 }; 
 
-function memoryFunctions(pressedMemoryFunction) {
+function setMemoryFunction(pressedMemoryFunction) {
     if (pressedMemoryFunction === 'M+') {
         memoryValue += parseFloat(initialValue);
-        display.innerText = memoryValue.toString();
     } else if (pressedMemoryFunction === 'M-') {
         memoryValue += (-1 * parseFloat(initialValue));
-        display.innerText = memoryValue.toString();
     } else {
-        display.innerText = memoryValue;
         initialValue = memoryValue.toString();
         memoryRecalled = true;
     };
-};
 
-function reset() {
-    initialValue = '';
-    finalValue = '';
-    operation = NaN;
-    state = 'initial';
+    display.innerText = memoryValue.toString();
 };
